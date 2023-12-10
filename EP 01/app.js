85a22b6ff759ee2e1411915ddf0f03411399ef2b
ankitji0811 <ankitjijindal030@gamil.com>
2023-12-10 19:57:59 +0530

    inital commit

---
+++ EP 01/app.js
@@ -0,0 +1,24 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },[
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Hello I am h1 Tag"),
+    React.createElement("h2", {}, "Hello I am h2 Tag"),
+  ]), 
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Hello I am h1 Tag"),
+    React.createElement("h2", {}, "Hello I am h2 Tag"),
+  ]), 
+]);
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello World from React"
+// );
+
+// console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
